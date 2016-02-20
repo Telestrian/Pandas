77a390c72581300e440d1c6dd22a7c08a77caf2d
Wolfbyte Studio <wolfbyteInteractive@gmail.com>
2016-02-20 19:48:25 +0000

    Some Gui

---
+++ ColourSwatcher/ColourSwatcher/Category.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColourSwatcher
+{
+    /// <summary>
+    /// Interaction logic for Category.xaml
+    /// </summary>
+    public partial class Category : UserControl
+    {
+
+
+        public static readonly int MAX = 39;
+
+        MainWindow owner;
+
+        private string category;
+
+        public string _Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+
+
+        public Category(string category)
+        {
+            InitializeComponent();
+            this.category = category;
+            this.owner = MainWindow.Instance;
+            this.lblName.Content = category;
+        }
+
+        public void add(PaletteColor col)
+        {
+            if(stkColours.Children.Count >= 39)
+            {
+                MessageBox.Show("You cannot add anymore colours for this pallet", 
+                    "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                stkColours.Children.Add(new PalleteColourControl(col));
+            }
+        }
+
+
+    }
+}
--- ColourSwatcher/ColourSwatcher/PaletteColor.cs
+++ ColourSwatcher/ColourSwatcher/PaletteColor.cs
@@ -1,54 +1,71 @@
-using System.Windows.Media.Color;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
 
-public class PaletteColor 
+namespace ColourSwatcher
 {
-	private Color color;
-	private bool favorite;
-	private PaletteColor (Color color)
-	{
-		this.color = color;
-	}
-	
-	public static PaletteColor parse(string parse)
-	{
-		try 
-		{
-			if(parse.contains(","))
-			{
-				int R = 0;
-				int G = 0; 
-				int B = 0;
+    public class PaletteColor
+    {
+        private Color color;
+        private bool favorite;
 
-				parse.replace(" ","");
-				parse.split(",");
-				R = int.Parse(parse[0]);
-				G = int.Parse(parse[1]);
-				B = int.Parse(parse[2]);
-				return new PaletteColor(Color.fromRGB(R, G, B));
-			}
-			else
-			{
-				Color color = (Color)ColorConverter.ConvertFromString(parse);
-				return new PaletteColor(color);
-			}
-			
-		}
-		catch 
-		{
-			throw new Exception("Eorror");
-		}
-		
-	}
-	public void setFavorite (bool favorite)
-	{
-		this.favorite = favorite;
-	}
-	public bool isFavorite ()
-	{
-		return favorite;
-	}
-	public Color getColor ()
-	{
-		return color;
-	}
+        private PaletteColor(Color color)
+        {
+            this.color = color;
+        }
+
+        public static PaletteColor parse(string parse)
+        {
+            try
+            {
+                if (parse.Contains(","))
+                {
+                    byte R = 0;
+                    byte G = 0;
+                    byte B = 0;
+
+                    parse.Replace(" ", "");
+                    string[] seg = parse.Split(',');
+                    R = byte.Parse(seg[0]);
+                    G = byte.Parse(seg[1]);
+                    B = byte.Parse(seg[2]);
+                    return new PaletteColor(Color.FromRgb(R, G, B));
+                }
+                else
+                {
+
+                    string newCol = parse.Replace("#", "");
+
+                    Color color = (Color)ColorConverter.ConvertFromString("#" + newCol);
+
+                    return new PaletteColor(color);
+                }
+
+            }
+            catch
+            {
+                throw new Exception("Eorror");
+            }
+
+        }
+
+        public void setFavorite(bool favorite)
+        {
+            this.favorite = favorite;
+        }
+
+        public bool isFavorite()
+        {
+            return favorite;
+        }
+
+        public Color getColor()
+        {
+            return color;
+        }
+
+    }
 }
